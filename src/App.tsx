560eea406223174f38b6f2b48f8280f992cd09c8
rohanvarmma29 <rohangadddam95@gmail.com>
2024-08-30 21:44:42 -0400

    tailwind-setup

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,11 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1 className="text-5xl font-bold">Overwatch</h1>
+    </div>
+  );
 }
 
 export default App;
