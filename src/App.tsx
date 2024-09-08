d080253bd1bce025dae383dd2c72c7f0ca8d7f32
Pavan Kalyan Konudula <pkonudul@uncc.edu>
2024-09-08 12:41:45 -0400

    Pull setup-layout changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,19 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [showSidebar, setShowSidebar] = useState(false)
 
   return (
-    <div className="App">
-      <h1 className="text-5xl font-bold">Overwatch</h1>
-    </div>
+    <>
+    <div className='App'>
+       <button className='side-bar-btn' onClick={() =>setShowSidebar(!showSidebar)}>
+           &lt;
+           </button>
+       {showSidebar && <header className="App-header">
+        <p>Hello Overwatch</p>
+      </header>}
+      </div>
+    </>
   );
 }
 
